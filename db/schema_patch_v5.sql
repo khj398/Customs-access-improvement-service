95f28e0e41303f2812a0627cf46ef3173c9cf4c5
MoKo <kingkevin717@gmail.com>
2026-07-26 16:06:51 +0900

    위치 추가

--- db/schema_patch_v5.sql
+++ db/schema_patch_v5.sql
@@ -6,9 +6,9 @@
 
 USE customs_auction;
 
-UPDATE customs_office SET latitude = 37.2636000, longitude = 127.0286000 WHERE cstm_sgn = '021'; -- 수원세관 (수원시 중심 근사치)
-UPDATE customs_office SET latitude = 35.3350000, longitude = 129.1378000 WHERE cstm_sgn = '033'; -- 양산세관 (양산시 중심 근사치)
-UPDATE customs_office SET latitude = 37.3943000, longitude = 126.9568000 WHERE cstm_sgn = '131'; -- 안양세관 (안양시 중심 근사치)
+UPDATE customs_office SET latitude = 37.2636000, longitude = 127.0286000 WHERE cstm_sgn = '021'; -- 수원세관
+UPDATE customs_office SET latitude = 35.3350000, longitude = 129.1378000 WHERE cstm_sgn = '033'; -- 양산세관 
+UPDATE customs_office SET latitude = 37.3943000, longitude = 126.9568000 WHERE cstm_sgn = '131'; -- 안양세관 
 
 -- 확인용: 좌표 없는(NULL) 세관이 남아있는지 점검
 -- SELECT cstm_sgn, cstm_name, latitude, longitude FROM customs_office WHERE latitude IS NULL;
